dee185717a031d440212bf6f8979f90ed75a21c9
evg <evgfast@gmail.com>
2016-09-16 22:03:07 +0300

    I added new test's method in GroupCreationTests and I created new class GroupData

---
+++ addressbook-web-tests/src/test/java/com/addressbook/GroupData.java
@@ -0,0 +1,25 @@
+package com.addressbook;
+
+public class GroupData {
+    private final String group_name;
+    private final String header;
+    private final String comment;
+
+    public GroupData(String group_name, String header, String comment) {
+        this.group_name = group_name;
+        this.header = header;
+        this.comment = comment;
+    }
+
+    public String getGroup_name() {
+        return group_name;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+}
