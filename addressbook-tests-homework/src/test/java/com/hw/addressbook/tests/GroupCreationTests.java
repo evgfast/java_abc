1e3138fe64386e165566d1c39b52deb524544bee
evg <evgfast@gmail.com>
2016-11-16 00:01:32 +0300

    read/write from xml

--- addressbook-tests-homework/src/test/java/com/hw/addressbook/tests/GroupCreationTests.java
+++ addressbook-tests-homework/src/test/java/com/hw/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,7 @@ package com.hw.addressbook.tests;
 import com.hw.addressbook.model.GroupData;
 import com.hw.addressbook.model.Groups;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -10,6 +11,8 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -28,8 +31,22 @@ public class GroupCreationTests extends TestBase{
         }
         return list.iterator();
     }
+    @DataProvider
+    public Iterator<Object[]> validXmlGroups() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")) );
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null){
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(GroupData.class);
+        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
 
-    @Test(dataProvider = "validGroups")
+    @Test(dataProvider = "validXmlGroups")
     public void testsGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
